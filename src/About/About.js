879d1e22c7f612036c2f139ecc74ae558ca7434c
Quievreux <jquievreux.devpro@gmail.com>
2021-11-22 15:39:09 +0100

    update about part

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './About.scss';
 
+import maggle from "../img/maggle.jpg"
+
 function About() {
     return (
+        <div className="about-main-container">
+
         <section className="about-container" id="about">
             <div className="about-left-block">
                 <h1>Bonjour, je suis Jérémy QUIEVREUX.<br/>Developpeur Fullstack JS Junior</h1>
@@ -13,9 +17,10 @@ function About() {
                     de type bootcamp au Bocal Academy Nice </p>
             </div>
             <div className="about-right-block">
-                <h1>Right Block</h1>
+                <img src={maggle} alt="" />
             </div>
         </section>
+        </div>
     )
 }
 
