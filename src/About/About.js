ec67402487a3e3f8b2fdd5611fd0b88bbb7eddcf
Quievreux <jquievreux.devpro@gmail.com>
2021-11-24 20:20:18 +0100

    About update

--- src/About/About.js
+++ src/About/About.js
@@ -9,14 +9,15 @@ function About() {
 
         <section className="about-container" id="about">
             <div className="about-left-block">
-                <h1>Bonjour, je suis Jérémy QUIEVREUX.<br/>Developpeur Fullstack JS Junior</h1>
+                <h1>Bonjour, je suis Jérémy</h1>
+                <h1>Developpeur Fullstack Javascript Junior</h1>
                 <p>Ancien Assistant Manager et passionné d'informatique depuis toujours , je me suis 
                     lancé dans une reconvertion profesionnelle dans le domaine du 
-                    Developpement Web/Mobile en 2019.</p>
+                    Developpement Web / Mobile en 2019.</p>
                     <p>J'ai commencé à me former en autodidacte grace à des sites comme 
                         Grafikart, OpenClassRoom, Udemy. J'ai ensuite intégrer le  centre de 
                         formation du Bocal Academy Nice, pour y suivre la formation de 
-                        Developpeur Fullstack Javascript
+                        Developpeur Fullstack Javascript.
                     </p>
             </div>
             <div className="about-right-block">
