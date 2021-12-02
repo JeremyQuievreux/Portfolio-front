d20d6cd7e1ce8be77563fb67f2bac69dbb6861f3
Quievreux <jquievreux.devpro@gmail.com>
2021-12-03 00:21:42 +0100

    update admin page

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,11 +1,27 @@
 import './Admin.scss';
 
-import React from 'react'
+import { BsFillShieldLockFill } from "react-icons/bs";
+
 
 function Admin() {
+
+
     return (
-        <div>
-            <h1>Admin Page</h1>
+        <div className="admin-page-main-container">
+            <div className="admin-page-title">
+                <BsFillShieldLockFill/>
+                <h1>Admin Page</h1>
+                <BsFillShieldLockFill/>
+            </div>
+            <div className="form-admin-container">
+                <form className="form-admin">
+                    <div className="form-admin-line">
+                        <label htmlFor="password">Admin Password : </label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <button>Valider</button>
+                </form>
+            </div>
         </div>
     )
 }
