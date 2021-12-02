176805e7668bb905a3c604066cb46c97390210eb
Quievreux <jquievreux.devpro@gmail.com>
2021-12-02 22:55:15 +0100

    add admin files

---
+++ src/Admin/Admin.js
@@ -0,0 +1,13 @@
+import './Admin.scss';
+
+import React from 'react'
+
+function Admin() {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+        </div>
+    )
+}
+
+export default Admin
