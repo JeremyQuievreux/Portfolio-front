4b5221b9836f09382cb89045c9bd79720234b5b3
Quievreux <jquievreux.devpro@gmail.com>
2021-12-03 14:56:38 +0100

    Update Admin.js

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 
 
 import { BsFillShieldLockFill } from "react-icons/bs";
-import { useState , useEffect} from 'react';
+import { useState } from 'react';
 
 
 function Admin() {
