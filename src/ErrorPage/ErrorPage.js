0b729c8e71ff5c2e568f921cef7b96813c730517
Quievreux <jquievreux.devpro@gmail.com>
2021-12-08 20:24:40 +0100

    add roadmap page / update 404 page

--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import './ErrorPage.scss';
 function ErrorPage() {
     return (
-        <div>
-            <h1>404 not found</h1>
+        <div className="error-page-container">
+            <div className="top-bar">
+            </div>
+            <div className="error-message-container">
+                <h1>Erreur 404</h1>
+                <p>Cette page n'existe pas</p>
+            </div>
+            <div className="bottom-bar">
+            </div>
         </div>
     )
 }
--- src/RoadMap/RoadMap.js
+++ src/RoadMap/RoadMap.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './RoadMap.scss'
 
 import roadmap from '../img/roadmap.png';
 
 function RoadMap() {
     return (
-        <div>
+        <div className="roadmap-container">
             <img src={roadmap} alt="" />
         </div>
     )
