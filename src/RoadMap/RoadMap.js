b4e6361de198fa12e8596757ab6b474365a00a63
Quievreux <jquievreux.devpro@gmail.com>
2021-12-08 20:35:47 +0100

    update scss roadmap

--- src/RoadMap/RoadMap.js
+++ src/RoadMap/RoadMap.js
@@ -6,7 +6,12 @@ import roadmap from '../img/roadmap.png';
 function RoadMap() {
     return (
         <div className="roadmap-container">
-            <img src={roadmap} alt="" />
+            <div className="roadmap-title">
+                <h1>My React Dev Roadmap</h1>
+            </div>
+            <div className="roadmap-image">
+                <img src={roadmap} alt="" />
+            </div>
         </div>
     )
 }
