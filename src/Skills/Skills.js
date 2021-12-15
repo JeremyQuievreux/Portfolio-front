8063d88954e13ddfdd587c6f73fdd9b22fc4d765
Quievreux <jquievreux.devpro@gmail.com>
2021-12-15 09:39:24 +0100

    change CV and update skill

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -45,6 +45,10 @@ function Skills() {
                             <BsCheckSquareFill/>
                             <p>Vue.JS</p>
                         </div>
+                        <div className="skills-item">
+                            <BsCheckSquareFill/>
+                            <p>Redux ( ... in progress )</p>
+                        </div>
                     </div>
                     <div className="skills-underblock">
                         <h3><FiDatabase/>Backend</h3>
